84eecffb488c80c731a5f29d5e64fcafa0d76736
Matteo <outlaw.plz@gmail.com>
2024-06-24 00:03:13 +0200

    Adds messages templates

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Reservation;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 
@@ -18,10 +19,13 @@ class MessageController extends Controller
         $messages = $reservation->messages ?? [];
 
         $message = $request->validate(self::rules());
+        /** @var User $authUser */
+        $authUser = $request->user();
 
         $message += [
             'ulid' => Str::ulid(),
-            'email' => $request->user()->email,
+            'name' => $authUser->name,
+            'email' => $authUser->email,
             'created_at' => now(),
         ];
 
