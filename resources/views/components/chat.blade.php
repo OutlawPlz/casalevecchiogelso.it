81305354ef8b18497a44adf4f75ad27935ad074c
Matteo <outlaw.plz@gmail.com>
2024-08-12 23:51:03 +0200

    Fixes images preview

--- resources/views/components/chat.blade.php
+++ resources/views/components/chat.blade.php
@@ -54,6 +54,8 @@
                     this.errors = {};
 
                     this.$refs.form.reset();
+
+                    this.previews = [];
                 })
                 .catch((error) => {
                     if (error.response.status === 422) {
@@ -165,27 +167,30 @@
             x-on:submit.prevent="submit"
             class="p-3 rounded-lg bg-white shadow space-y-2"
         >
-            <div class="w-full text-sm text-gray-900 bg-white">
-                <textarea
-                    name="message"
-                    rows="1"
-                    x-model="message"
-                    class="block w-full p-0 border-0 focus:ring-0"
-                    placeholder="{{ __('Your message') }}..."
-                ></textarea>
+            <textarea
+                name="message"
+                rows="1"
+                x-model="message"
+                class="block w-full py-1 px-2 border-0 focus:ring-0"
+                placeholder="{{ __('Your message') }}..."
+            ></textarea>
 
+            <template x-if="previews.length">
                 <div class="flex space-x-2">
                     <template x-for="src in previews">
-                        <div class="relative cursor-pointer mt-2 -translate-x-2">
-                            <img :src="src" alt="Preview" class="rounded-lg w-24 h-24 object-cover hover:opacity-30">
-
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
-                            </svg>
-                        </div>
+                        <img :src="src" alt="Preview" class="rounded-lg w-24 h-24 object-cover">
                     </template>
+
+                    <div
+                        x-on:click="$refs.fileInput.value = ''; $refs.fileInput.dispatchEvent(new Event('change'));"
+                        class="flex flex-row-reverse items-center h-24 bg-gray-100 px-3 rounded-lg text-gray-500 cursor-pointer hover:text-gray-900 hover:bg-gray-200"
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                        </svg>
+                    </div>
                 </div>
-            </div>
+            </template>
 
             <div class="flex w-full">
                 @host
@@ -195,8 +200,8 @@
                             type="button"
                             class="inline-flex justify-center p-2 text-gray-500 rounded-lg cursor-pointer hover:text-gray-900 hover:bg-gray-100"
                         >
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5">
-                                <path fill-rule="evenodd" d="M10 2c-2.236 0-4.43.18-6.57.524C1.993 2.755 1 4.014 1 5.426v5.148c0 1.413.993 2.67 2.43 2.902 1.168.188 2.352.327 3.55.414.28.02.521.18.642.413l1.713 3.293a.75.75 0 0 0 1.33 0l1.713-3.293a.783.783 0 0 1 .642-.413 41.102 41.102 0 0 0 3.55-.414c1.437-.231 2.43-1.49 2.43-2.902V5.426c0-1.413-.993-2.67-2.43-2.902A41.289 41.289 0 0 0 10 2ZM6.75 6a.75.75 0 0 0 0 1.5h6.5a.75.75 0 0 0 0-1.5h-6.5Zm0 2.5a.75.75 0 0 0 0 1.5h3.5a.75.75 0 0 0 0-1.5h-3.5Z" clip-rule="evenodd" />
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
                             </svg>
                         </button>
                     </x-slot>
@@ -223,6 +228,7 @@
                     </svg>
 
                     <input
+                        x-ref="fileInput"
                         type="file"
                         name="media[]"
                         multiple
