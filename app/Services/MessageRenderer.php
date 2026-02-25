1f0f6ed3c94c185de27336813462434b122e66a1
Matteo <outlaw.plz@gmail.com>
2026-02-25 20:36:02 +0100

    wip

--- app/Services/MessageRenderer.php
+++ app/Services/MessageRenderer.php
@@ -3,8 +3,10 @@
 namespace App\Services;
 
 use App\Models\Message;
+use App\Models\Reservation;
 use DeepL\DeepLException;
 use DeepL\Translator as DeepLClient;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
 
 use function App\Helpers\is_template;
@@ -14,7 +16,7 @@ class MessageRenderer
     public function __construct(protected DeepLClient $translator) {}
 
     /**
-     * @param  array<string, mixed>  $data
+     * @param  array{language: string, reservation: Reservation}  $data
      */
     public function render(Message $message, array $data = []): string
     {
@@ -28,19 +30,32 @@ class MessageRenderer
             ? $this->renderTemplate($message, $data)
             : $this->renderContent($message);
 
+        // If $language is an empty string, we're showing the
+        // message to its author, no translation needed.
         if (! $language) {
             return $renderedContent;
         }
 
-        try {
-            $renderedContent = $this->translator->translateText($renderedContent, null, $language)->text;
+        $renderedContent = $this->translate($renderedContent, $language);
 
-            $message->update(["content->$language" => $renderedContent]);
+        $message->update(["content->$language" => $renderedContent]);
+
+        return $renderedContent;
+    }
+
+    protected function translate(string $text, string $target): string
+    {
+        try {
+            $translatedContent = $this->translator->translateText($text, null, $target);
         } catch (DeepLException $exception) {
             report($exception);
         }
 
-        return $renderedContent;
+        if (! isset($translatedContent) || ! strncasecmp($translatedContent->detectedSourceLang, $target, 2)) {
+            return $text;
+        }
+
+        return $translatedContent->text;
     }
 
     protected function renderContent(Message $message): string
